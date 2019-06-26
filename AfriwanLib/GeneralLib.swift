8f14c5f0441119eb8e413985c12d6087d43d42c9
Afriwan Ahda <afriwan.phys@gmail.com>
2019-06-26 15:02:19 +0700

    update

---
+++ AfriwanLib/GeneralLib.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralLib.swift
+//  AfriwanLib
+//
+//  Created by Afriwan Ahda on 26/06/19.
+//  Copyright © 2019 AfriwanAhda. All rights reserved.
+//
+
+import Foundation
+
+public final class GeneralLib {
+    
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+}
