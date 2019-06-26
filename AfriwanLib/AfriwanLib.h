6043c9606d1a70723ee9ce20522d8f46145baf13
Afriwan Ahda <afriwan.phys@gmail.com>
2019-06-26 14:21:52 +0700

    Initial Commit

---
+++ AfriwanLib/AfriwanLib.h
@@ -0,0 +1,19 @@
+//
+//  AfriwanLib.h
+//  AfriwanLib
+//
+//  Created by Renom on 26/06/19.
+//  Copyright © 2019 AfriwanAhda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AfriwanLib.
+FOUNDATION_EXPORT double AfriwanLibVersionNumber;
+
+//! Project version string for AfriwanLib.
+FOUNDATION_EXPORT const unsigned char AfriwanLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AfriwanLib/PublicHeader.h>
+
+
