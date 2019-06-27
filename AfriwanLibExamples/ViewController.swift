54a3c7d1fc991eb337e2929a85afdaf1c972b148
Afriwan Ahda <afriwan.phys@gmail.com>
2019-06-27 19:30:17 +0700

    update

--- AfriwanLibExamples/ViewController.swift
+++ AfriwanLibExamples/ViewController.swift
@@ -10,31 +10,25 @@ import UIKit
 import AfriwanLib
 
 class ViewController: UIViewController {
-    @IBOutlet weak var lb_test: UILabel!
-    @IBOutlet weak var view_test: UIView!
-    @IBOutlet weak var button_test: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        roundCorner(view: view_test, cornerRadius: 15)
-////        roundCorner(view: view_test, cornerRadius: 15, shadowOpacity: 0.3, shadowRadius: 2.0, shadowOffset: CGSize.init(width: 0, height: 1), masksToBounds: false)
-//
-//        roundCorner(view: button_test, cornerRadius: button_test.frame.height/2)
-////        roundCorner(view: button_test, cornerRadius: 20, shadowOpacity: 0.7, masksToBounds: true)
-//
-////        pulsate(view: view_test)
-////        pulsate(view: button_test)
-//
-//        shake(view: view_test)
-//        flash(view: button_test)
-        
+        setupView()
     }
     
     private func setupView() {
-        
+        //        roundCorner(view: view_test, cornerRadius: 15)
+        ////        roundCorner(view: view_test, cornerRadius: 15, shadowOpacity: 0.3, shadowRadius: 2.0, shadowOffset: CGSize.init(width: 0, height: 1), masksToBounds: false)
+        //
+        //        roundCorner(view: button_test, cornerRadius: button_test.frame.height/2)
+        ////        roundCorner(view: button_test, cornerRadius: 20, shadowOpacity: 0.7, masksToBounds: true)
+        //
+        ////        pulsate(view: view_test)
+        ////        pulsate(view: button_test)
+        //
+        //        shake(view: view_test)
+        //        flash(view: button_test)
     }
-
-
+    
 }
 
