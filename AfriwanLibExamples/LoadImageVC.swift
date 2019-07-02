d14e978a79dd2c33dee4305588fd309af95185d6
Afriwan Ahda <afriwan.phys@gmail.com>
2019-07-02 17:52:25 +0700

    update

--- AfriwanLibExamples/LoadImageVC.swift
+++ AfriwanLibExamples/LoadImageVC.swift
@@ -27,7 +27,7 @@ class LoadImageVC: UIViewController {
         roundCorner(view: image_bottom, cornerRadius: image_bottom.bounds.width/2, masksToBounds: true)
         
         // Simple load image
-        image_top.loadImage(imageUrl: "https://media.licdn.com/dms/image/C5116AQH_xL0QdZUR1A/profile-displaybackgroundimage-shrink_350_1400/0?e=1567641600&v=beta&t=FuMK2dM15ffsMFsGu19q6Zg_FzE4xnPzC2yfeXO7Z4k")
+        image_top.loadImage(imageUrl: "https://github.com/AfriwanAhda/AfriwanLib/blob/master/demo/sky.jpg")
         
         // Load image with background image before image fully loaded
         image_bottom.loadImage(imageUrl: "https://avatars0.githubusercontent.com/u/33817855?s=400&u=4c3c536e97c52bf1108604b53e6d46a58b55d426&v=4", backgroundImage: #imageLiteral(resourceName: "AppIcon"))
