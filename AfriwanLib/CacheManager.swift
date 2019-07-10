317e0ebab915998920d9ecaf2b5a731c484d5d78
Afriwan Ahda <afriwan.phys@gmail.com>
2019-07-10 22:24:31 +0700

    update

--- AfriwanLib/CacheManager.swift
+++ AfriwanLib/CacheManager.swift
@@ -6,6 +6,10 @@
 //  Created by Afriwan Ahda on 01/07/19.
 //  Copyright © 2019 AfriwanAhda. All rights reserved.
 //
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
 
 class CacheManager: NSObject {
     private var cache: NSCache<NSString,UIImage>?
